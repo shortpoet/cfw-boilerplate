62656ab2f55bd67a3417b6f8548bb898e403a6d4
Carlos Soriano <soriano.carlitos@gmail.com>
2023-12-14 09:16:35 -0500

    tasks!

--- app/api/src/index.ts
+++ app/api/src/index.ts
@@ -1,13 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import { ExecutionContext } from '@cloudflare/workers-types';
 import { MethodNotAllowedError, NotFoundError } from '@cloudflare/kv-asset-handler/dist/types';
 import {
