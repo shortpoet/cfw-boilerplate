465c411b40f5b730d5448c4a46ca1ca8ad0c2c83
Carlos Soriano <soriano.carlitos@gmail.com>
2023-12-14 09:12:43 -0500

    task

--- app/api/src/router/routes/task.ts
+++ app/api/src/router/routes/task.ts
@@ -1,20 +1,13 @@
 import { IRequest } from 'itty-router';
 import { OpenAPIRouter } from '@cloudflare/itty-router-openapi';
 
-import { TaskList } from '@/api/src/controllers';
-// import { TaskCreate } from '@/api/src/controllers';
-// import { TaskFetch } from '@/api/src/controllers';
-// import { TaskDelete } from '@/api/src/controllers';
-// router.get('/api/tasks/', TaskList);
-// router.post('/api/tasks/', TaskCreate);
-// router.get('/api/tasks/:taskSlug/', TaskFetch);
-// router.delete('/api/tasks/:taskSlug/', TaskDelete);
-
-type CF = [env: Env, ctx: ExecutionContext];
+import { TaskCreate, TaskDelete, TaskFetch, TaskList } from '@/api/src/controllers';
+type CF = [env: Env, ctx: ExecutionContext, data: Record<string, any>];
 const router = OpenAPIRouter<IRequest, CF>({ base: '/api/task' });
 
-// router.get('/tasks', TaskList);
-
 router.get('/tasks', TaskList);
+router.post('/tasks/', TaskCreate);
+router.get('/tasks/:taskSlug/', TaskFetch);
+router.delete('/tasks/:taskSlug/', TaskDelete);
 
 export default router;
