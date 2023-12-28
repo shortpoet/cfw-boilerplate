8102e996a2707643b13b4b6cc4c53b821bfc7383
Carlos Soriano <soriano.carlitos@gmail.com>
2023-12-27 21:45:26 -0500

    gen

--- app/ui/src/services/AuthService.ts
+++ app/ui/src/services/AuthService.ts
@@ -2,32 +2,33 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-import type { AuthLoginEmailBody } from '../models/AuthLoginEmailBody'
-import type { AuthLoginUsernameBody } from '../models/AuthLoginUsernameBody'
-import type { AuthRegisterBody } from '../models/AuthRegisterBody'
-import type { Session } from '../models/Session'
+import type { AuthLoginEmailBody } from '../models/AuthLoginEmailBody';
+import type { AuthLoginUsernameBody } from '../models/AuthLoginUsernameBody';
+import type { AuthRegisterBody } from '../models/AuthRegisterBody';
+import type { Session } from '../models/Session';
 
-import type { CancelablePromise } from '../core/CancelablePromise'
-import { OpenAPI } from '../core/OpenAPI'
-import { request as __request } from '../core/request'
+import type { CancelablePromise } from '../core/CancelablePromise';
+import { OpenAPI } from '../core/OpenAPI';
+import { request as __request } from '../core/request';
 
 export class AuthService {
+
   /**
    * Log in via your email and password
    * @returns Session User Object
    * @throws ApiError
    */
   public static postLoginPasswordUser({
-    requestBody
+    requestBody,
   }: {
-    requestBody?: AuthLoginEmailBody | AuthLoginUsernameBody
+    requestBody?: (AuthLoginEmailBody | AuthLoginUsernameBody),
   }): CancelablePromise<Session> {
     return __request(OpenAPI, {
       method: 'POST',
       url: '/api/auth/login/password',
       body: requestBody,
-      mediaType: 'application/json'
-    })
+      mediaType: 'application/json',
+    });
   }
 
   /**
@@ -36,15 +37,16 @@ export class AuthService {
    * @throws ApiError
    */
   public static postRegisterPasswordUser({
-    requestBody
+    requestBody,
   }: {
-    requestBody?: AuthRegisterBody
+    requestBody?: AuthRegisterBody,
   }): CancelablePromise<Session> {
     return __request(OpenAPI, {
       method: 'POST',
       url: '/api/auth/register/password',
       body: requestBody,
-      mediaType: 'application/json'
-    })
+      mediaType: 'application/json',
+    });
   }
+
 }
